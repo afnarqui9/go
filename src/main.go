8768d1c9e6a71992000ed4db49a5dc554e377eb1
andres naranjo <andres.naranjo@banistmo.com>
2023-11-19 12:41:24 -0500

    paquete fmt

--- src/main.go
+++ src/main.go
@@ -5,25 +5,25 @@ import (
 )
 
 func main(){
-	const pi float64 = 3.14
-	const pi2 = 3.14
-	fmt.Println("pi: ", pi)
-	fmt.Println("pi2: ", pi2)
+	// const pi float64 = 3.14
+	// const pi2 = 3.14
+	// fmt.Println("pi: ", pi)
+	// fmt.Println("pi2: ", pi2)
 
-	base := 12
-	var altura int = 14
-	var area  int
+	// base := 12
+	// var altura int = 14
+	// var area  int
 
-	fmt.Println("base: ", base)
-	fmt.Println("altura: ", altura)
-	fmt.Println("area: ", area)
+	// fmt.Println("base: ", base)
+	// fmt.Println("altura: ", altura)
+	// fmt.Println("area: ", area)
 
-	var a int
-	var b float64
-	var c string
-	var d bool
+	// var a int
+	// var b float64
+	// var c string
+	// var d bool
 
-	fmt.Println(a,b,c,d)
+	// fmt.Println(a,b,c,d)
 
 //Numeros enteros
 //int = Depende del OS (32 o 64 bits)
@@ -51,4 +51,11 @@ func main(){
 //Complex64 = Real e Imaginario float32
 //Complex128 = Real e Imaginario float64
 //Ejemplo : c:=10 + 8i	
+nombre := "vienen"
+valores := 100
+fmt.Printf("%s muchos afnarqui %d aja \n", nombre, valores)
+message := fmt.Sprintf("%s muchos %d aja \n", nombre, valores)
+fmt.Println(message)
+fmt.Printf("type: %T", valores)
+
 }
