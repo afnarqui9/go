48d1a8efd5717dcf5967cc651b12352da8ecec5b
andres naranjo <andres.naranjo@banistmo.com>
2023-11-19 15:42:40 -0500

    array

--- src/main.go
+++ src/main.go
@@ -2,44 +2,28 @@ package main
 
 import "fmt"
 
-func contadorVocales(palabra string)(int, int, int, int, int,){
-	conta := 0
-	conte := 0
-	conti := 0
-	conto := 0
-	contu := 0
-	for _ , valor := range palabra{ 
-		variable := string(valor)
-		switch variable {
-		case "a" :
-			conta++
-		case "e" :
-			conte++
-		case "i" :
-			conti++
-		case "o" :
-			conto++
-		case "u" :
-			contu++
-		}
-	}
-
-	fmt.Println("Inicio")
-	for i := 0; i < 5; i++ {
-	defer fmt.Println(i)
- 	}
- 	fmt.Println("Fin")	
-	return conta, conte , conti, conto, contu
-}
-
 func main() {
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
 
-	palabra := "paramos de aprender"	
-	a,e,i,o,u := contadorVocales(palabra)
-	fmt.Printf("la frase '%s' tiene: \n",palabra)
-	fmt.Printf("%d vocales a \n",a)
-	fmt.Printf("%d vocales e \n",e)
-	fmt.Printf("%d vocales i \n",i)
-	fmt.Printf("%d vocales o \n",o)
-	fmt.Printf("%d vocales u \n",u)	
+	// Slice
+	slice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
+	fmt.Println(slice, len(slice), cap(slice))
+
+	// Métodos en el slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
+
+	// Append
+	slice = append(slice, 11)
+	fmt.Println(slice)
+
+	// Append
+	newSlice := []int{12, 13, 14}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
 }
---
+++ src/switch.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func contadorVocales(palabra string)(int, int, int, int, int,){
+	conta := 0
+	conte := 0
+	conti := 0
+	conto := 0
+	contu := 0
+	for _ , valor := range palabra{ 
+		variable := string(valor)
+		switch variable {
+		case "a" :
+			conta++
+		case "e" :
+			conte++
+		case "i" :
+			conti++
+		case "o" :
+			conto++
+		case "u" :
+			contu++
+		}
+	}
+
+	fmt.Println("Inicio")
+	for i := 0; i < 5; i++ {
+	defer fmt.Println(i)
+ 	}
+ 	fmt.Println("Fin")	
+	return conta, conte , conti, conto, contu
+}
+
+func main() {
+
+	palabra := "paramos de aprender"	
+	a,e,i,o,u := contadorVocales(palabra)
+	fmt.Printf("la frase '%s' tiene: \n",palabra)
+	fmt.Printf("%d vocales a \n",a)
+	fmt.Printf("%d vocales e \n",e)
+	fmt.Printf("%d vocales i \n",i)
+	fmt.Printf("%d vocales o \n",o)
+	fmt.Printf("%d vocales u \n",u)	
+}
